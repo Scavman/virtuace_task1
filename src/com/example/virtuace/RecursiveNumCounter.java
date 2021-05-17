1ee9904044f8e3ab78a185a051ed5386aa4e5872
Scav <giorgi.gedevanidze.1@gmail.com>
2021-05-17 19:46:22 +0400

    Made minor adjustments in RecursiveNumCounter class

--- src/com/example/virtuace/RecursiveNumCounter.java
+++ src/com/example/virtuace/RecursiveNumCounter.java
@@ -5,14 +5,12 @@ public class RecursiveNumCounter {
     private static final int startingNumber = 1;
     private int startingNum = startingNumber;
 
-    public RecursiveNumCounter() {
-    }
-
     public void count(int num){
         System.out.println(startingNum);
         startingNum++;
         if(num >= startingNum)
             count(num);
-        startingNum = startingNumber;
+        else
+            startingNum = startingNumber;
     }
 }
